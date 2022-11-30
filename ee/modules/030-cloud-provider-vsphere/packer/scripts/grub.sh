004ebe6a828683bb94cb87fa21eea12fadd4b61c
Maksim Kiselev <maksim.kiselev@flant.com>
2022-11-30 10:12:14 +0300

    fixes for build ubuntu22 packer image with cloudinit

---
+++ ee/modules/030-cloud-provider-vsphere/packer/scripts/grub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# These scripts were modified from boxcutter
+
+set -e
+set -x
+
+rm -f /etc/cloud/cloud.cfg.d/99-installer.cfg
+rm -f /etc/cloud/cloud.cfg.d/subiquity-disable-cloudinit-networking.cfg
+echo 'disable_vmware_customization: false' | tee -a /etc/cloud/cloud.cfg
+sed -i 's|nocloud-net;seedfrom=http://.*/|vmware|' /etc/default/grub
+update-grub
+cloud-init clean
+
